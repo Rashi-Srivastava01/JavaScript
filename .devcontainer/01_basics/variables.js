8af7ea59f32751176735e1278b420f698085accb
Rashi-Srivastava01 <rashisrivastava934@gmail.com>
2025-01-30 17:59:03 +0000

    container config done

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,18 @@
+const id=12345
+let name="Rashi"
+let email="rashi@gmail.com"
+var password ="232323145"
+city="lucknow"
+
+// id=9876 // not allowed because const can not be re-declared and updated
+
+email="shi@gmail.com"
+password="1256953"
+
+/*
+we not use var anymore as it issue in functional scope and it
+can be re-declared and updated.
+let keyword and not be re-decared but can be updated
+*/
+
+console.table([id,name,email,password,city])
