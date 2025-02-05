644af32b3da9a534ca3d5bfabcca97f6316f9053
Rashi-Srivastava01 <rashisrivastava934@gmail.com>
2025-02-05 10:08:31 +0000

    container config done

---
+++ Datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // It has some syntax as we are using nodejs , not a browser
+
+let name="Rashi"//string
+let age= 19 //number
+let isLoggedIn = false //boolean
+let n; // undefined
+let b= null 
+
+/*
+number range => 2^53
+bigint
+string => ""
+boolean => true or false
+null => standalone value
+undefined => means value is not defined yet
+symbol => to identify unique components
+*/
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
+
