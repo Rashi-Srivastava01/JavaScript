fec02d713074f9743508e2094fcc3eb140727f8b
Rashi-Srivastava01 <rashisrivastava934@gmail.com>
2025-02-07 11:48:11 +0000

    conversion of datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,24 @@
+let age = undefined//string
+console.log(typeof age)
+
+let changeInNumber = Number(age)
+console.log(changeInNumber) //12
+console.log(typeof changeInNumber)
+
+//"12" =>12
+//"12abc" =>NaN (not a number)
+// null => 0
+//true =>1 ; false =>0
+//undefined => NaN
+
+let isLoggedIn ="rashi"
+
+let changeInBoolean = Boolean(isLoggedIn)
+console.log(changeInBoolean)
+
+//"rashi" => true
+// 1 => true ; 0 => false
+//"" => false
+
+
+
