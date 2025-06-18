29ee1f4b2fb5c93ee1a3fdbb69dff9f736626835
Rashi-Srivastava01 <rashisrivastava934@gmail.com>
2025-06-18 15:24:42 +0530

    Commit

---
+++ operation.js
@@ -0,0 +1,17 @@
+let value= 5
+let negValue= -value
+// console.log(negValue);//-5
+
+let str1="hello"
+let str2=" rashi"
+console.log(str1+str2);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32  //It is a guideline that primitive value gets priority
+
+console.log(+true)//1
+console.log(+"")//0
+console.log(+false)//0
